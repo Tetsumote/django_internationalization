7875b986da05500381ecbb46a075643dc7a9d3ea
Paweł Grajewski <unionproject88@gmail.com>
2019-11-02 03:22:14 +0100

    add basic stracture

--- internationalizationProject/settings.py
+++ internationalizationProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'translateApp.apps.TranslateappConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
